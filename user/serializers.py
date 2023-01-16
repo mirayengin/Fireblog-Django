66652d936f72b17e08c53663d2e2f3059cd49145
erhan <erhanserkansabriye@gmail.com>
2023-01-16 14:09:12 +0300

    feat : tokenserializer yazıldı

--- user/serializers.py
+++ user/serializers.py
@@ -2,7 +2,7 @@ from .models import MyUser
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
-           
+from dj_rest_auth.serializers import TokenSerializer
 
 # class MyUserSerializer(serializers.ModelSerializer):
 #     seller_product_count = serializers.SerializerMethodField()
@@ -15,29 +15,54 @@ from django.contrib.auth.password_validation import validate_password
 #         return obj.products.count()
 
 
-class RegisterUserSerializer(serializers.ModeiSerializer):
+class RegisterUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     # email = serializers.EmailField(required=True, validators=[UniqueValidator])
     class Meta:
         model = MyUser
-        fields = ("username","email", "first_name", "last_name", "role")
+        fields = ("username","email", "first_name", "last_name", "role", "password", "password2")
 
     def validate(self, data):
-        if data.get("password") == data["password2"]:
+        if data["password"] != data["password2"]:
             raise serializers.ValidationError(
                 {"password2": "Passwords must match"}
             )
         return data
     
+    # def create(self, validated_data):
+    #     validated_data.pop("password2")
+    #     user = super().create(**validated_data)
+    #     return user
+    
     def create(self, validated_data):
         password = validated_data["password"]
         password2 = validated_data.pop("password2")
-        user = MyUser.objects.creater(**validated_data)
+        user = MyUser.objects.create(**validated_data)
         user.set_password(password)
         user.save()
         return user
-    
+
+class UserTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = MyUser
+            fields = ("id", "username", "email")
+
+
+class CustomTokenSerializer(TokenSerializer):
+     user = UserTokenSerializer(read_only=True)
+
+     class Meta(TokenSerializer):
+          fields = ("key","user")
+
+
+
+
+
+
+
+
+
 
 
 
